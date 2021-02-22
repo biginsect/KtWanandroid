40c6b215ed221c2e42f689fc3f052355c8bb44c2
lipeng <lipeng19@xiaomi.com>
2021-02-22 20:49:56 +0800

    create

---
+++ app/src/main/java/com/biginsect/ktwanandroid/constant/Constants.kt
@@ -0,0 +1,11 @@
+package com.biginsect.ktwanandroid.constant
+
+/**
+ *@author lipeng
+ *Created at 2021/2/22 20:16
+ */
+object Constants {
+
+
+    const val BASE_URL = "http://wanandroid.com/"
+}
